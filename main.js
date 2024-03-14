98f6c1080da823d3e846c592875cf1abf3e62c09
Nanobuo <nanobu88@gmail.com>
2024-03-14 02:43:37 +0100

    Turned pet card into a JS template

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const template = document.querySelector("#pet-card-template");
+
+const wrapper = document.createDocumentFragment();
+
 async function start() {
   const weatherPromise = await fetch(
     "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
@@ -19,9 +23,15 @@ async function petsArea() {
 
   const petsData = await petsPromise.json();
 
-  petsData.forEach(() => {
-    console.log("Hello");
+  petsData.forEach((pet) => {
+    const clone = template.content.cloneNode(true);
+
+    clone.querySelector("h3").textContent = pet.name;
+
+    wrapper.appendChild(clone);
   });
+
+  document.querySelector(".list-of-pets").appendChild(wrapper);
 }
 
 petsArea();
