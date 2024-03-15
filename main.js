6a48e28f7c289f126633be61428228959f1ccc28
Nanobuo <nanobu88@gmail.com>
2024-03-15 01:27:39 +0100

    Added active styling to current filter button

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const template = document.querySelector("#pet-card-template");
-
 const wrapper = document.createDocumentFragment();
 
 async function start() {
@@ -52,4 +51,20 @@ function createAgeText(birthYear) {
   if (age == 1) return "1 year old";
   if (age == 0) return "Less than a year old";
   return `${age} years old`;
+}
+
+// pet filter button code
+const allButtons = document.querySelectorAll(".pet-filter button");
+allButtons.forEach((el) => {
+  el.addEventListener("click", handleButtonClick);
+});
+
+function handleButtonClick(e) {
+  // remove active class from any and all buttons
+  allButtons.forEach((el) => el.classList.remove("active"));
+
+  // add active class to the specific button that just got clicked
+  e.target.classList.add("active");
+
+  // actually filter the pets down below
 }
